5f8dec6583bb393e51b024a63b4b7475b72be58f
LaiviAC <ciserariel@gmail.com>
2026-03-04 14:36:54 +0200

    Update iev_kalkulators.py
    Pievienoju funkcijas

--- iev_kalkulators.py
+++ iev_kalkulators.py
@@ -1,4 +1,14 @@
 # Programmas kods
+
+Calc():
+#...
+
+Izmaksa():
+#...
+
+Jam():
+#...
+
 a = float(input("Lūdzu, ievadi ābolu svaru:")
 b = float(input("2.	Lūdzu, ievadi cukura cenu (par 1 kg):")
 print(f"Jums ir nepieciešams {Calc(a)} kg cukura, kas maksā {Izmaksa(b)} EUR. Gala ievārījuma masa būs {Jam(a)} kg.")
